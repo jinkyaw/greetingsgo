2a81a7f34f871662be4ae7de0363bbd21a12bc18
Ajinkya Wankhade <ajinkyawankhade90@gmail.com>
2021-12-14 11:26:34 +0530

    fixed empty strin issue

--- greetings.go
+++ greetings.go
@@ -7,8 +7,8 @@ import (
 
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
-	if name == '' {
-		return '', errors.New("empty name")
+	if name == "" {
+		return "", errors.New("empty name")
 	}
 
     // Return a greeting that embeds the name in a message.
